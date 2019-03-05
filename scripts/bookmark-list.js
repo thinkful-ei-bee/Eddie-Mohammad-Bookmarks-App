1f4f6b665dd6a6f47d2936d2e76f9ee6e9474b99
Eddie Bueno <eddiebueno93@gmail.com>
2019-03-05 14:34:05 -0800

    started generate functions

--- scripts/bookmark-list.js
+++ scripts/bookmark-list.js
@@ -1,1 +1,42 @@
 // generate function
+'use strict';
+
+/*global $, api, store*/
+
+const bookmarkList = (function(){
+
+  /******* Generate Functions *******/
+
+  function generateBookmark(bookmark){
+    // this function generate the html element version of the provided bookmark in a string
+
+  }
+
+  function generateBookmarkList(list){
+    // this function generates one string
+    const listCopy = list.map(item => generateBookmark(item));
+    return listCopy.join('');
+  }
+
+  function generateExtendedViewBookmark(bookmark){
+    // this function generates the extended view of the provided bookmark
+
+  }
+
+  function generateSubmitForm(){
+    // this function generates the form to add the bookmark
+  }
+
+  function generateMenu(){
+    // this function generates the add bookmark button and dropdown. Returns html in a string
+  }
+
+  /******** Render Function *******/
+
+
+  /******* Handle Functions *******/
+
+  return {
+
+  };
+}());
