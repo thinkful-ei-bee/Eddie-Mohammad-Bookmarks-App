31ff3d35cafd5bb23a99706bb5c78436716dcfeb
Eddie Bueno <eddiebueno93@gmail.com>
2019-03-06 22:29:47 -0800

    refactored error handling, added more comments to functions

--- scripts/bookmark-list.js
+++ scripts/bookmark-list.js
@@ -5,7 +5,7 @@
 
 const bookmarkList = (function(){
 
-  /******* Generate Functions *******/
+  /************************************************ Generate Functions ********************************************/
 
   function generateBookmark(bookmark){
     // this function generate the html element version of the provided bookmark in a string
@@ -90,8 +90,9 @@ const bookmarkList = (function(){
     `;
   }
 
-  /******** Render Function *******/
+  /************************************************** Render Functions ********************************************/
   function render(){
+    // this function renders the information provided by the store
     if (store.error){
       $('.error-display').html(generateError());
     }
@@ -105,10 +106,10 @@ const bookmarkList = (function(){
     $('.list-controls').html(generateMenu());
   }
 
-  /******* Handle Functions *******/
+  /************************************************** Handle Functions ********************************************/
   function handleAddBookmarkClick(){
     // this function opens the add bookmark form and removes the add button and filter options
-    $('.list-controls').on('click','#js-add-bookmark-btn',function(event){
+    $('.list-controls').on('click','#js-add-bookmark-btn',function(){
       store.adding = true;
       render();
     });
@@ -132,18 +133,7 @@ const bookmarkList = (function(){
       $('#description').val('');
       $('input[name=star]:checked').val('');
       api.createBookmark(title,url,desc,rating)
-        .then(res=>{
-          if (!res.ok) {
-            console.log(`This is the error codet: ${res.status}`);
-            store.error = {code: res.status};
-          }
-          if (!res.headers.get('content-type').includes('json')) {
-            console.log(`This is the res.statusText: ${res.statusText}`);
-            store.error.message = res.statusText;
-            return Promise.reject(store.error);
-          }
-          return res.json();
-        })
+        .then(handleError)
         .then(data =>{
           if (store.error){
             console.log(`This is the error message: ${data.message}`);
@@ -161,43 +151,98 @@ const bookmarkList = (function(){
   }
 
   function handleCancelClick(){
-    $('.list-controls').on('click','.js-cancel-button',function(event){
+    // this function changes the state of the store so that it is not adding anything
+    // when render is run again the submit form will be removed
+    $('.list-controls').on('click','.js-cancel-button',function(){
       store.adding = false;
       render();
     });
   }
 
   function captureId(element){   
+    // this function gets the data id from the parameter element
+    // data id was added in the generate bookmark function
+    // returns id
     return $(element).data('id');
   }
 
   function handleToggleBookmarkView(){
+    // this function toggles the detailed view of a bookmark
+    // toggles the bookmarks extended attribute
     $('.bookmark-list').on('click','.bookmark', function(event){
-      // console.log(event.currentTarget);
       const id= captureId(event.currentTarget);
       const bookmark= store.findById(id);
       bookmark.extended = !bookmark.extended; // Changed the store
       render();
     });
   }
+  
+  function handleError(res){
+    // this function checks the response from the thinkful api for any errors
+    // returns a promise
+    if (!res.ok) {
+      console.log(`This is the error code: ${res.status}`);
+      store.error = {code: res.status};
+    }
+    if (!res.headers.get('content-type').includes('json')) {
+      console.log(`This is the res.statusText: ${res.statusText}`);
+      store.error.message = res.statusText;
+      return Promise.reject(store.error);
+    }
+    return res.json();
+  }
+
+  function isEmpty(obj){
+    // this function checks an object if it is empty
+    // returns a boolean
+    return Object.hasOwnPropertyNames(obj).length> 0;
+  }
+
+  function updateStore(data){
+    // this function will update our store based off of the api data from the thinkful api
+    // if the data returned is an empty object, we assume it was a DELETE request 
+    // otherwise we will assume it was a POST
+    
+    if (isEmpty(data)){
+      // in the case of DELETE
+      if(store.error){
+        store.error.message= 'Cannot delete an item';
+      }
+      else{
+        //TODO: Find Solution
+        // current problem we do not have access to id in order to delete it from our store
+        // need to figure out how to access id
+        // possible solution, we add a property to our store to keep track of the 
+        // id of the element we are focusing on
+        //store.deleteBookmark(id);
+      }
+    }
+    else{
+      // in the case of POST
+      if (store.error){
+        store.error.message = data.message;
+      }
+      else{
+        store.addBookmark(data);
+        store.adding = false;
+      } 
+    }
+  }
 
   function handleDeleteBookmarkClick(){ //event delegation
+    // this function will delete a bookmark
+    // find the id associated with the bookmark to be deleted
+    // call the api to delete it
+    // from the response, make sure no errors occured
+    // delete the bookmark from store
+    // re render
+    // clear the error in the store
     $('.bookmark-list').on('click', '.js-delete-button', function(event){      
       const id = captureId($(event.currentTarget).parents('li'));
       api.deleteBookmark(id)
-        .then(res => {
-          if (!res.ok) {
-            console.log(`This is the error code: ${res.status}`);
-            store.error = {code: res.status};
-          }
-          if (!res.headers.get('content-type').includes('json')) {
-            console.log(`This is the res.statusText: ${res.statusText}`);
-            store.error.message = res.statusText;
-            return Promise.reject(store.error);
-          }
-          res.json();
-        })
+        .then(handleError)
         .then((data) => {
+          console.log(data,typeof data);
           if(store.error){
             store.error.message= 'Cannot delete an item';
             console.log(store.error.message);
@@ -210,9 +255,8 @@ const bookmarkList = (function(){
   }
   
   function handleMinimumRatingChange(){ // event delegation
+    // this function keeps track of the minimum rating change
     $('.list-controls').on('change', 'select', function(event){
-      console.log('rating changed');
-      console.log($(this).val());
       store.setMinimumRating( $(this).val());
       render();      
     });
